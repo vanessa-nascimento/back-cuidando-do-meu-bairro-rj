b42cc74ce46567d7a04de6eb0813bac247b181fc
Vanessa Nascimento <vanascimento.dev@gmail.com>
2022-12-17 13:36:11 -0300

    feat: add config deploy app

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./api/data/dataset-rio.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./api/data/dataset-rio.json')
 
-server.listen(port);
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'api/data/dataset-rio.json', {
+    _isFake: isProductionEnv
+})
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
+
+server.use(router)
+server.listen(process.env.PORT || 3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
