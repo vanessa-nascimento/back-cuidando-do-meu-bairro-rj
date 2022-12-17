f172afdb80ba8bd3256d55876e755911b3c83290
Vanessa Nascimento <vanascimento.dev@gmail.com>
2022-12-17 13:13:30 -0300

    feat: remove node_module and create the datebase

--- api/spend/datasource/spend.js
+++ api/spend/datasource/spend.js
@@ -4,6 +4,10 @@ class SpendsAPI extends RESTDataSource {
     constructor() {
         super()
         this.baseURL = 'http://localhost:3000'
+        this.customResponse = {
+            code: 200,
+            message: "Sucesso!"
+        }
     }
 
     async getSpends() {
@@ -16,11 +20,25 @@ class SpendsAPI extends RESTDataSource {
     
     async addSpends(spend) {
         const spends = await this.get('/data')
-        spend.id = spend.Exercicio + spend.Orgao + (spends.length + 1)
-        await this.post('data', {...spend})
+        spend.id = spend.spend.Exercicio + spend.spend.Orgao + (spends.length + 1)
+        spend.spend.updatedAt = new Date().toLocaleString(),
+        await this.post('data', {...spend.spend})
         return({
-            ...spend
+            ...this.customResponse,
+            spend: {
+                ...spend.spend
+            }
         })
+    }
+
+    async deleteSpend(id) {
+        await this.delete(`/data/${id}`)
+        return this.customResponse
+    }
+
+    async deleteAllSpends() {
+        await this.delete('/data')
+        return this.customResponse
     }
 }
 
--- api/spend/resolvers/spendResolvers.js
+++ api/spend/resolvers/spendResolvers.js
@@ -1,12 +1,23 @@
+const  { GraphQLScalarType } = require('graphql')
+
 const spendResolvers = {
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'String de data e hora no formato ISO-8601',
+        serialize: (value) => value.toISOString(),
+        parseValue: (value) => new Date(value),
+        parseLiteral: (ast) => new Date(ast.value)
+    }),
     Query: {
-        spends: (root, args, { dataSources }, info) => dataSources.
+        spends: (root, args, { dataSources }) => dataSources.
         spendsAPI.getSpends(),
         spend: (root, { id }, { dataSources }) => dataSources.spendsAPI.getSpendById(id)
     },
-    
+
     Mutation: {
-        addSpends: async (root, spend, { dataSources }) => dataSources.spendsAPI.addSpends(spend)
+        addSpends: async (root, spend, { dataSources }) => dataSources.spendsAPI.addSpends(spend),
+        deleteSpend: async (root, { id }, { dataSources }) => dataSources.spendsAPI.deleteSpend(id),
+        deleteSpends: async (root, args, { dataSources }) => dataSources.spendsAPI.deleteAllSpends()
     }
 }
 
